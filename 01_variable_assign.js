02510771abfac4e16725523af351a65250295128
SanjayRede <redesanjay86@gmail.com>
2022-12-08 23:05:04 +0530

    variable js
    variable js

---
+++ 01_variable_assign.js
@@ -0,0 +1,14 @@
+var myFullName
+myFullName=("Sanjay Rede")
+console.log(myFullName);
+
+var myFullName
+myFullName=("Sanjay chandrakant Rede")
+console.log(myFullName);
+
+var mylove
+mylove=("i love only JS")
+console.log(mylove);
+
+var firstname=("Sanjay"),lastname=("Rede"),Age=("26"),address=("Atp venegaon Tal Madha Dist Solapur"),pincode=("413211"),gender=("male"),married=("yes")
+console.log(firstname,lastname,Age,address,pincode,gender,married);
