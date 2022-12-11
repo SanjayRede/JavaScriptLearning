ba819bdb522d1e773da1033397fe166bf6aaba1a
SanjayRede <redesanjay86@gmail.com>
2022-12-11 23:51:09 +0530

    function code
    function code

---
+++ 01_funtions_assign.js
@@ -0,0 +1,40 @@
+console.log("1.functions without argument no return");
+function ilove() {
+  console.log("i love java script");
+  console.log("i am angular develolper");
+}
+ilove();
+
+console.log("2.functions with argument no return");
+
+function personalDetails(firstName, lastName, collegeName) {
+  console.log(firstName);
+  console.log(lastName);
+  console.log(collegeName);
+}
+personalDetails(
+  "Sanjay",
+  "Rede",
+  "Skn sinhgad college of engineering korti pandharpur"
+);
+
+console.log("3.Swapping function");
+function swapValuesDude(value1,value2) {
+    console.log("before swap ");
+    console.log(value1,value2);
+    var temp=value1;
+    value1=value2;
+    value2=temp;
+    console.log("after swap");
+    console.log(value1,value2);
+}
+swapValuesDude("Virat", "Anushka");
+swapValuesDude( 1000,2000);
+
+console.log("4.function with arguments &return");
+function addThreeValues(value1,value2,value3) {
+    console.log(value1+value2+value3);
+    return addThreeValues;
+ }
+ addThreeValues(10.23,600,40)
+ addThreeValues("Hello", "good", "morning")
---
+++ 02_normalFunction.js
@@ -0,0 +1,7 @@
+function bankDetails(bankName,accountNum,location,Pincode) {
+    console.log("Bank Name:CITI BANK",   "Account NO:3456782345", "location:pune", "pincode:411202");
+    console.log("Bank Name:Axix BANK",   "Account NO:7856782345", "location:mumbai", "pincode:441202");
+    console.log("Bank Name:HDFC BANK",   "Account NO:8956782345", "location:pune", "pincode:631202");
+
+}
+bankDetails();
