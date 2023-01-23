72267dbc89343ae267f3de3e743be312b4600167
SanjayRede <redesanjay86@gmail.com>
2023-01-23 06:59:33 +0530

    update output fact
    update output fact

--- 04_outputFact.js
+++ 04_outputFact.js
@@ -5,3 +5,22 @@ console.log(`null==undefined= ${null==undefined}`);
 console.log(`1==[1]= ${1==[1]}`);
 console.log(`1==true= ${1==true}`);
 console.log(`1=='1'= ${1=='1'}`);
+
+console.log(`==================`);
+console.log(5==`5`);
+
+console.log(`5`===5);
+// console.log(5==5);
+console.log(5===5);
+
+console.log(`====`);
+var num1=15;
+var num2=`15`;
+var result=num1===num2;
+console.log(result);
+console.log(`==================`);
+console.log(2==true);
+console.log(`=========`);
+console.log(1==[1]);
+
+console.log();
